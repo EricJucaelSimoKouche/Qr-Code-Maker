be1fb5bb3bbf21a8bb8d58e3bbea3320a26f2309
EricJucaelSimoKouche <simojucael@gmail.com>
2025-07-09 10:55:48 +0200

    Qr Code generator

---
+++ script.js
@@ -0,0 +1,24 @@
+let qrCodeContent = document.getElementById('qr-code-content');
+let myform = document.getElementById("my-form");
+let qrImgBox = document.getElementById("qr-image-box");
+let qrImg = document.getElementById('qr-image')
+
+let add = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
+
+function qrCodeMaker(qrCodeContent) {
+    qrImg.src = add + qrCodeContent.value;
+}
+
+myform.addEventListener('submit', ($event) => {
+    if (qrCodeContent.value.length === 0 ) {
+        qrCodeContent.classList.add('error')
+        setTimeout(() => {
+            qrCodeContent.classList.remove("error")
+        }, 1000)
+    } else {
+        $event.preventDefault();
+        qrCodeMaker(qrCodeContent);
+        qrImgBox.classList.add("show-image");
+        myform.reset();
+    }
+})
